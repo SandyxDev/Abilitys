cc0f6390ccd4f1c31c577133de52014f3d97a008
zVind <105098542+zVind@users.noreply.github.com>
2022-05-07 02:05:30 -0500

    Update Main.php

--- Main.php
+++ Main.php
@@ -2,4 +2,41 @@
 
 declare{strixt_types=1}
 
-namespace 
+namespace LatamTeam\hcf\Main;
+
+use LataTeam\hcf\Commands\tems;
+use LatamTeam\hcf\Events\AbilitysEvents;
+use pocketmine\item\enchantment\EnchantmentInstance;
+
+use pocketmine\item\enchantment\VanillaEnchantments;
+
+use pocketmine\item\ItemFactory;
+
+use pocketmine\item\VanillaItems;
+use pocketmine\player\Player;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+
+  private static $instance;
+  public $hitSessions = [];
+  /** @var array<string,true>*/
+  public $idSessions = [];
+  
+  public array $pumpkin = [];
+  public array $apple = [];
+  public array $poket = [];
+  public array $closecall = []:
+
+	
+  public array $global = [];
+
+
+
+
+
+
+
+
+}
